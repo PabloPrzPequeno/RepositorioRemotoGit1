acff5e7e72a58f7642e37c8ee6578e0f60fff6b0
pablo <pablo@DESKTOP-EE57HI1>
2024-01-16 13:19:34 +0100

    Prueba1

---
+++ Proyecto1/src/Paquete1/Main.java
@@ -0,0 +1,9 @@
+package Paquete1;
+
+public class Main {
+public static void main(String [] args) {
+	// Todo Auto-generated method stub
+	System.out.println("Ejemplo de control de versiones con Git");
+	System.out.println("Segundo ejemplo de control de versiones con Git");
+   }
+}
